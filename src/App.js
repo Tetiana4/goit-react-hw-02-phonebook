f4ec1add69b34fc4b5ead732537c73cd88c5cd22
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-09-04 00:27:11 +0200

    Change App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-import './App.css';
+import React, { Component } from 'react';
+import Form from './Component/Form';
 
-function App() {
-  return (
-    <input
-      type="text"
-      name="name"
-      pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-      title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-      required
-    />
-  );
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  formSubmitHandler = data => {
+    console.log(data);
+  };
+
+  handleChange = event => {
+    const { filter, value } = event.currentTarget;
+
+    this.setState({
+      [filter]: value,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <Form propSubmit={this.formSubmitHandler} />
+        <h2>Contacts</h2>
+        <p>Find contacts by name</p>
+        <label id={this.name}>
+          <input type="text" value={this.state.filter} onChange={this.handleChange} name="filter" />
+        </label>
+        <ul>
+          {this.state.contacts.map(contact => (
+            <li key={contact.id}>
+              {contact.name}: {contact.number}
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
 }
-
 export default App;
