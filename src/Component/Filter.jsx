2cd3979f06198e79ed3bb81e117a1a7c2c31b9ca
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-09-04 23:20:38 +0200

    Change code

--- src/Component/Filter.jsx
+++ src/Component/Filter.jsx
@@ -1,18 +1,10 @@
-// import React from "react";
+import React from 'react';
 
-// const Filter = ({ value, onChange }) => {
-//     <>
+const Filter = ({ value, onChange }) => (
+  <label>
+    <input type="text" value={value} onChange={onChange} />
+  </label>
+);
+export default Filter;
 
-//         <label id={this.name}>
-//             <input type="text" value={value} onChange={onChange} name="filter" />
-//         </label>
-//         <ul>
-//             {this.state.contacts.map(contact => (
-//                 <li key={contact.id}>
-//                     {contact.name}: {contact.number}
-//                 </li>
-//             ))}
-//         </ul>
-//     </>
-// }
-// export default Filter;
+// <label id={this.name}>  06
