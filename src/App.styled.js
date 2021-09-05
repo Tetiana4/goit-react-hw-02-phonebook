1fc8531b52edf2dd167d29d523570d20b5803b95
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-09-05 14:15:54 +0200

    Add styles

---
+++ src/App.styled.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Div = styled.div`
+  margin: 0 auto;
+  width: 400px;
+  height: 500px;
+
+  text-align: center;
+  justify-content: center;
+  border-radius: 10px;
+  border: 3px solid #ce6a6a;
+  background-color: #ec9f9f;
+`;
---
+++ src/Component/ContactList/ContactList.styled.js
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const List = styled.ul`
+  list-style: none;
+`;
+
+export const Button = styled.button`
+  color: #f71c0c;
+  border: 1px solid #723a3a;
+  border-radius: 5px;
+`;
