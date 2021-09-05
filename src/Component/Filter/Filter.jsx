598326662d928adaa3934c7220805c03af1fbb65
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-09-05 14:16:07 +0200

    Add new folder

--- src/Component/Filter/Filter.jsx
+++ src/Component/Filter/Filter.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Filter = ({ value, onChange }) => (
   <label>
@@ -7,4 +8,7 @@ const Filter = ({ value, onChange }) => (
 );
 export default Filter;
 
-// <label id={this.name}>  06
+Filter.propeTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
