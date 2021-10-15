6ac1510dc779353e091f73a9b456abdfd96b9c46
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-10-15 14:17:27 +0200

    Change code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
-import App from './Component/App/App';
+import App from './components/App/App';
 import './index.css';
 
 ReactDOM.render(
@@ -10,5 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-reportWebVitals();
